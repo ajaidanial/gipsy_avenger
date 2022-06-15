9a2939d9c6f84387e399e70afa403b2a870938c0
ajaidanial <ajaidanial@gmail.com>
2022-06-16 01:38:12 +0530

    cleaned app files

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gipsy_avenger.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
