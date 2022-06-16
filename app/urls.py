0b9d310d94146eb555c6e39216dd74acb3aaf736
ajaidanial <ajaidanial@gmail.com>
2022-06-17 03:21:48 +0530

    finished ui tweaking

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,20 @@ from . import views
 urlpatterns = [
     path("server/ping/", views.server_ping_view),
     path("dashboard/", views.DashboardPageView.as_view(), name="dashboard_page_view"),
+    # scheduled request
+    path(
+        "requests/",
+        views.AlterEC2RequestListView.as_view(),
+        name="request_list_page_view",
+    ),
+    path(
+        "requests/create/",
+        views.AlterEC2RequestCreateView.as_view(),
+        name="request_create_page_view",
+    ),
+    path(
+        "requests/<pk>/delete/",
+        views.AlterEC2RequestDeleteView.as_view(),
+        name="request_delete_page_view",
+    ),
 ]
