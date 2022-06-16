efeb64db675c854242133570baa676afa86a1916
ajaidanial <ajaidanial@gmail.com>
2022-06-16 06:21:28 +0530

    added some more

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from app.models import AlterEC2Log
 
-def server_ping_view(request):
+
+def server_ping_view(request):  # noqa
     """Just ping the server."""
 
     return HttpResponse("Pong!")
@@ -12,3 +14,10 @@ class DashboardPageView(TemplateView):
     """Landing page after authentication for the user."""
 
     template_name = "page_dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        """Necessary context."""
+
+        data = super().get_context_data(**kwargs)
+        data.update({"logs": AlterEC2Log.objects.select_related("for_request")})
+        return data
