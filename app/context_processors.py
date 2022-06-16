5f38ed51ff2fa0472e944963e4b1588dd5ca6f2e
ajaidanial <ajaidanial@gmail.com>
2022-06-17 00:28:10 +0530

    added layout

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,3 +1,6 @@
+from django.urls import reverse_lazy
+
+
 def template_common(request):
     """Common context for the templates."""
 
@@ -5,4 +8,8 @@ def template_common(request):
         "app_title": "Gipsy Avenger",
         "my_portfolio_url": "https://ajaidanial.wtf",
         "my_username": "ajaidanial",
+        "navbar_links": [
+            {"display": "Dashboard", "url": reverse_lazy("dashboard_page_view")},
+            {"display": "Requests", "url": "#"},
+        ],
     }
