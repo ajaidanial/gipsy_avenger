4737860590737a16fc3a8d8b2ca9b33d88b6e6af
ajaidanial <ajaidanial@gmail.com>
2022-06-17 01:22:00 +0530

    added highlight

--- app/context_processors.py
+++ app/context_processors.py
@@ -9,7 +9,15 @@ def template_common(request):
         "my_portfolio_url": "https://ajaidanial.wtf",
         "my_username": "ajaidanial",
         "navbar_links": [
-            {"display": "Dashboard", "url": reverse_lazy("dashboard_page_view")},
-            {"display": "Requests", "url": reverse_lazy("request_list_page_view")},
+            {
+                "display": "Dashboard",
+                "url": reverse_lazy("dashboard_page_view"),
+                "highlight": "dashboard",
+            },
+            {
+                "display": "Requests",
+                "url": reverse_lazy("request_list_page_view"),
+                "highlight": "requests",
+            },
         ],
     }
