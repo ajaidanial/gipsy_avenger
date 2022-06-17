a5ddb7e1a79dea09fd66281bb83f9b36f096fbd5
ajaidanial <ajaidanial@gmail.com>
2022-06-17 15:56:29 +0530

    added static files support for admin panel

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,3 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")),
 ]
+
+# Static Files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
