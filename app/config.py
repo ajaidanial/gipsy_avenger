f7126764e1daa408de52bfe0fd2ab686ed8a66e8
ajaidanial <ajaidanial@gmail.com>
2022-06-17 06:27:29 +0530

    added some more

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 DAYS_OF_WEEK = [
-    "sunday",  # 0
-    "monday",  # 1
-    "tuesday",  # 2
-    "wednesday",  # 3
-    "thursday",  # 4
-    "friday",  # 5
-    "saturday",  # 6
+    "monday",  # 0
+    "tuesday",  # 1
+    "wednesday",  # 2
+    "thursday",  # 3
+    "friday",  # 4
+    "saturday",  # 5
+    "sunday",  # 6
 ]
 DAYS_OF_WEEK_CHOICES = [(i, v.upper()) for i, v in enumerate(DAYS_OF_WEEK)]
